18a5795580c108460ce46054db463f90ce29c552
Luismarin2510 <opraedu@gmail.com>
2022-10-16 17:36:45 -0500

    cargando nueva pantalla con sus respectivas densidades

---
+++ app/src/main/java/com/lemarin31/aventurateporcolombia/welcomActivity.kt
@@ -0,0 +1,11 @@
+package com.lemarin31.aventurateporcolombia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class welcomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcom)
+    }
+}
