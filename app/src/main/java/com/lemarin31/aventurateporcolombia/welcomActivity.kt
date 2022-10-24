4904465b44eb17ef8d42a40b762ac01cc736eaec
Luismarin2510 <opraedu@gmail.com>
2022-10-23 22:38:14 -0500

    mejoras en mensajes emergentes

--- app/src/main/java/com/lemarin31/aventurateporcolombia/welcomActivity.kt
+++ app/src/main/java/com/lemarin31/aventurateporcolombia/welcomActivity.kt
@@ -1,11 +1,13 @@
 package com.lemarin31.aventurateporcolombia
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AlertDialogLayout
 
 class welcomActivity : AppCompatActivity() {
     private var edtusername : EditText?=null
@@ -16,13 +18,33 @@ class welcomActivity : AppCompatActivity() {
         edtusername = findViewById(R.id.edtusername)
         edtpassword = findViewById(R.id.edtpassword)
     }
-    fun destino(btndestino : View){
-            if (edtusername!!.text.toString()=="admin" && edtpassword!!.text.toString()=="12345"){
-                val intent= Intent(this,Maindestination::class.java).apply {  }
-                startActivity(intent)}
-        else{
-            val botondialogo = AlertDialog.Builder(this).setTitle("Usuario o contraseña invalidos").setMessage("Ingrese usuario y o contraseña correcta").create().show()
-        }
-    }
 
+    val Mensajeok ={ dialog: DialogInterface,which:Int->
+        val intent= Intent(this,Maindestination::class.java).apply {  }
+        startActivity(intent)
+    }
+    val Mensajecancel={_:DialogInterface,_:Int->}
+
+
+    fun destino(btndestino : View){
+            if (edtusername!!.text.toString()=="admin" && edtpassword!!.text.toString()=="12345") {
+                //val intent= Intent(this,Maindestination::class.java).apply {  }
+                //startActivity(intent)}
+                val dialog = AlertDialog.Builder(this)
+                    .setTitle(resources.getString(R.string.welcome))
+                    .setMessage(edtusername!!.text.toString())
+                    .setPositiveButton("Ok", Mensajeok)
+                    .create()
+                    .show()
+            }
+             else{
+                 val botondialogo = AlertDialog.Builder(this)
+                     .setTitle(resources.getString(R.string.Erroruser))
+                     .setMessage(resources.getString(R.string.Errormenssage))
+                     .setNegativeButton("OK",Mensajecancel)
+                     .create()
+                     .show()
+                }
+
+    }
 }
