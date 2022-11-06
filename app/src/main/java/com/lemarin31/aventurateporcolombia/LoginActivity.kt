33920718c058c0f759451041c66f8aa765ed147c
Luismarin2510 <opraedu@gmail.com>
2022-11-06 12:07:47 -0500

    configuraciones para firebase

--- app/src/main/java/com/lemarin31/aventurateporcolombia/LoginActivity.kt
+++ app/src/main/java/com/lemarin31/aventurateporcolombia/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.lemarin31.aventurateporcolombia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+
 import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
@@ -11,9 +12,13 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
     }
 
+
     fun basedatos(btnlogin : View){
-        Toast.makeText(applicationContext,getResources().getString(R.string.tex_baselogin),
-            Toast.LENGTH_LONG)
-            .show()
+
+        //Toast.makeText(applicationContext,getResources().getString(R.string.tex_baselogin),Toast.LENGTH_LONG).show()
+
+
     }
+
+
 }
