07cd51361525cd86671ed294501baa5acb5534c2
Luismarin2510 <opraedu@gmail.com>
2022-11-06 17:40:12 -0500

    prueba de firebase sin exito

--- app/src/main/java/com/lemarin31/aventurateporcolombia/LoginActivity.kt
+++ app/src/main/java/com/lemarin31/aventurateporcolombia/LoginActivity.kt
@@ -1,24 +1,51 @@
 package com.lemarin31.aventurateporcolombia
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-
+import android.widget.EditText
 import android.widget.Toast
 
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 class LoginActivity : AppCompatActivity() {
+    lateinit var correo :EditText
+    lateinit var contraseña :EditText
+    lateinit var contra :EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        correo = findViewById(R.id.editTextcorreo)
+        contraseña = findViewById(R.id.editcontraseña1)
+        contra = findViewById(R.id.editcontraseña2)
+
+
+      //  val analitica = FirebaseAnalytics.getInstance(this)
+      //  val paquete = Bundle()
+      //  paquete.putString("messaje","integracion completa ")
+      //  analitica.logEvent("Initcreen",paquete)
+
     }
 
+     fun guardar(btnguardar: View) {
 
-    fun basedatos(btnlogin : View){
+         val dbfireabse = FirebaseFirestore.getInstance()
 
-        //Toast.makeText(applicationContext,getResources().getString(R.string.tex_baselogin),Toast.LENGTH_LONG).show()
+             dbfireabse.collection("Usuario").
+             document().set(hashMapOf("correo" to correo.text.toString(),
+                 "contraseña" to contraseña.text.toString()))
+         Toast.makeText(applicationContext,"usuario registrado ", Toast.LENGTH_LONG).show()
+             finish()
 
-
-    }
-
+     }
 
 }
+
