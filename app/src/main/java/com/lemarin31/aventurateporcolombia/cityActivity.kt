a53c45767c0c2408446cbf6fe930c208c5dd56db
PabloTapasco <111782528+PabloTapasco@users.noreply.github.com>
2022-11-11 14:35:47 -0500

    .

--- app/src/main/java/com/lemarin31/aventurateporcolombia/cityActivity.kt
+++ app/src/main/java/com/lemarin31/aventurateporcolombia/cityActivity.kt
@@ -8,7 +8,6 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class cityActivity : Fragment() {
-    lateinit var binding: cityActivity
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +20,43 @@ class cityActivity : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val fragmento = inflater.inflate(R.layout.activity_city, container, false)
-        val detallefestividades: Button = fragmento.findViewById(R.id.buttonHist)
-        detallefestividades.setOnClickListener(View.OnClickListener {
+        val fragmento = inflater.inflate(R.layout.activity_cities, container, false)
+        val detallebog: Button = fragmento.findViewById(R.id.btnBog)
+        val detallemedellin: Button = fragmento.findViewById(R.id.buttonMed)
+        val detallecali: Button = fragmento.findViewById(R.id.buttonCali)
+        val detallecartagena: Button = fragmento.findViewById(R.id.buttonCarta)
+
+        detallebog.setOnClickListener(View.OnClickListener {
+
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.setReorderingAllowed(true)
+                ?.replace(R.id.cities, Activity_Festividades::class.java, null, "festividades")
+                ?.addToBackStack("")
+                ?.commit()
+
+
+        })
+        detallemedellin.setOnClickListener(View.OnClickListener {
+
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.setReorderingAllowed(true)
+                ?.replace(R.id.cities, Activity_Festividades::class.java, null, "festividades")
+                ?.addToBackStack("")
+                ?.commit()
+
+
+        })
+        detallecali.setOnClickListener(View.OnClickListener {
+
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.setReorderingAllowed(true)
+                ?.replace(R.id.cities, Activity_Festividades::class.java, null, "festividades")
+                ?.addToBackStack("")
+                ?.commit()
+
+
+        })
+        detallecartagena.setOnClickListener(View.OnClickListener {
 
             activity?.supportFragmentManager?.beginTransaction()
                 ?.setReorderingAllowed(true)
