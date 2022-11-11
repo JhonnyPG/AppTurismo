18138a232c0a323c236015fb545e01907e20e1e1
PabloTapasco <111782528+PabloTapasco@users.noreply.github.com>
2022-11-10 20:14:02 -0500

    .

---
+++ app/src/main/java/com/lemarin31/aventurateporcolombia/Activity_Festividades.kt
@@ -0,0 +1,18 @@
+package com.lemarin31.aventurateporcolombia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+class Activity_Festividades : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
--- app/src/main/java/com/lemarin31/aventurateporcolombia/cityActivity.kt
+++ app/src/main/java/com/lemarin31/aventurateporcolombia/cityActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class cityActivity : Fragment() {
+    lateinit var binding: cityActivity
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -17,8 +21,18 @@ class cityActivity : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val fragmento = inflater.inflate(R.layout.activity_city,container,false)
+        val fragmento = inflater.inflate(R.layout.activity_city, container, false)
+        val detallefestividades: Button = fragmento.findViewById(R.id.buttonHist)
+        detallefestividades.setOnClickListener(View.OnClickListener {
 
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.setReorderingAllowed(true)
+                ?.replace(R.id.cities, Activity_Festividades::class.java, null, "festividades")
+                ?.addToBackStack("")
+                ?.commit()
+
+
+        })
         return fragmento
     }
 }
