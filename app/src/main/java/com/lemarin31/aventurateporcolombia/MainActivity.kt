0f31e581a12db4eb23db535c7b041e43204c53b4
Luismarin2510 <opraedu@gmail.com>
2022-11-21 00:02:18 -0500

    primer splash screen  funcional

--- app/src/main/java/com/lemarin31/aventurateporcolombia/MainActivity.kt
+++ app/src/main/java/com/lemarin31/aventurateporcolombia/MainActivity.kt
@@ -5,11 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Thread.sleep(3000)
+        splashScreen.setKeepOnScreenCondition { false }
+
+
+
     }
     fun welcom(btncontinuar : View){
         val intent= Intent(this,welcomActivity::class.java).apply {  }
